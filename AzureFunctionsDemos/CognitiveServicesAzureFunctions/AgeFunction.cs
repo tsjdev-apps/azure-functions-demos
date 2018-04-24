dd69f871542b7a116749cda2cca6efea87d447dc
Sebastian Jensen <apps@tsjdev-apps.de>
2018-04-24 08:36:09 +0200

    - add Age function

---
+++ AzureFunctionsDemos/CognitiveServicesAzureFunctions/AgeFunction.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ProjectOxford.Face;
+
+namespace CognitiveServicesAzureFunctions
+{
+    public static class AgeFunction
+    {
+        [FunctionName("AgeFunction")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            try
+            {
+                log.Info("AgeFunction - Started");
+
+                var queryNameValuePairs = req.GetQueryNameValuePairs().ToList();
+
+                // get url and apikey
+                var url = queryNameValuePairs.FirstOrDefault(q => string.Compare(q.Key, "url", StringComparison.OrdinalIgnoreCase) == 0).Value;
+                var apiKey = queryNameValuePairs.FirstOrDefault(q => string.Compare(q.Key, "apikey", StringComparison.OrdinalIgnoreCase) == 0).Value;
+                var domainEndpoint = queryNameValuePairs.FirstOrDefault(q => string.Compare(q.Key, "domain", StringComparison.OrdinalIgnoreCase) == 0).Value;
+
+                dynamic data = await req.Content.ReadAsAsync<object>();
+
+                // get url from body
+                if (url == null)
+                    url = data?.url?.ToString();
+
+                // get apikey from body
+                if (apiKey == null)
+                    apiKey = data?.apikey?.ToString();
+
+                if (domainEndpoint == null)
+                    domainEndpoint = data?.domain?.ToString();
+
+                if (string.IsNullOrEmpty(url))
+                    return req.CreateResponse(HttpStatusCode.BadRequest, "No image provided");
+
+                if (string.IsNullOrEmpty(apiKey))
+                    return req.CreateResponse(HttpStatusCode.BadRequest, "No apikey provided");
+
+                if (string.IsNullOrEmpty(domainEndpoint))
+                    return req.CreateResponse(HttpStatusCode.BadRequest, "No domain endpoint provided");
+
+                log.Info($"AgeFunction - Url: {url}");
+                log.Info($"AgeFunction - ApiKey: {apiKey}");
+                log.Info($"AgeFunction - Endpoint: {domainEndpoint}");
+
+                // analyze image from url with the provided apikey
+                var service = new FaceServiceClient(apiKey, $"https://{domainEndpoint}.api.cognitive.microsoft.com/face/v1.0");
+                var faceAttributes = new[] { FaceAttributeType.Age };
+
+                var result = await service.DetectAsync(HttpUtility.UrlDecode(url), true, false, faceAttributes);
+                var emotionResult = result?.Select(r => new { r.FaceId, r.FaceRectangle, r.FaceAttributes.Age });
+
+                // send the result back
+                return req.CreateResponse(HttpStatusCode.OK, emotionResult);
+            }
+            catch (Exception e)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+    }
+}
